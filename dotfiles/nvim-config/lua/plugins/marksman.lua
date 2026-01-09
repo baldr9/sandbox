4c2d380bca681d7cbd6d972b868df4055e276470
Fabio Rojas <fabio.rojas@thermofisher.com>
2026-01-08 19:28:02 -0800

    use markdownlint.yaml

--- dotfiles/nvim-config/lua/plugins/marksman.lua
+++ dotfiles/nvim-config/lua/plugins/marksman.lua
@@ -1,5 +1,6 @@
 return {
 	-- Marksman LSP for navigation and completion
+	-- Use .markdownlint.yaml config file under project root
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -24,41 +25,6 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.marksman.setup({
 				capabilities = capabilities,
-			})
-		end,
-	},
-
-	-- Markdownlint for linting (MD rules)
-	{
-		"mfussenegger/nvim-lint",
-		optional = true,
-		opts = {
-			linters_by_ft = {
-				markdown = { "markdownlint" },
-			},
-		},
-		config = function()
-			-- markdownlint github.com/DavidAnson/markdownlint
-			-- Alternative: Use .markdownlint.yaml config file under project root
-			require("lint").linters.markdownlint.args = {
-				"--disable",
-				"MD013", -- Disable line length rule
-				-- Add more rules to disable:
-				-- "--disable", "MD033", -- Disable inline HTML
-				-- "--disable", "MD041", -- Disable first line heading
-			}
-
-			-- Keybinding to manually trigger linting
-			vim.keymap.set("n", "<leader>cl", function()
-				require("lint").try_lint()
-			end, { desc = "Lint current file" })
-
-			-- Auto-lint on save and text change
-			vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
-				pattern = { "*.md", "*.markdown" },
-				callback = function()
-					require("lint").try_lint()
-				end,
 			})
 		end,
 	},
